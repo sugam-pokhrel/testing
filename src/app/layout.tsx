9628f6e2f171c0f74f8730e551ec5c2dbf2ca084
Sugam Pokhrel <psugam75@gmail.com>
2023-09-15 09:24:48 +0545

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Navbar from './navbar'
+import Navbar from './Navbar'
 import Providers from './components/Providers'
 
 const inter = Inter({ subsets: ['latin'] })
